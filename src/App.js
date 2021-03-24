69d8b2ca23cdf89c030c180c37c859f1c05aaee4
Paula Krupa <paula.krupa@gmail.com>
2021-03-24 22:26:26 +0100

    henlo Kat

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './App.css';
 
+// let animal1 = prompt("Give me the name of an animal, real or imaginary:")
+//
+// let animal2 = prompt("Give me the name of another animal, real or imaginary:")
+//
+// let animal3 = prompt("Give me the name of the last animal, real or imaginary:")
+
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{color: 'red'}}>
+        {"{REST:API}"}
     </div>
   );
 }
